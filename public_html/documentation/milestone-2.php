3ff7e40b91e3cf741256e0edacf223179c25805a
Stephen Pelot <spelot@cnm.edu>
2018-10-25 16:28:53 -0600

    dear friends

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<div>
+			<h1>One Page Breakdown</h1>
+			<br>
+			<h2>Ahoy Mateys!</h2>
+			<p>This section will house my contact details, and a personal greeting, Javascript enabled interface
+			</p>
+			<br>
+			<h2>The Booty</h2>
+			<p>This is where I'll put my work, education, and professional history. I'll have my languages here, alongside perhaps a link to my favorite story. This will show both my proficiencies, as well as detailing a bit about who I am</p>
+			<br>
+			<h2>The Bay</h2>
+			<p>This is where I'll toss my links to Github and LinkedIn, perhaps my StackOverflow as well</p>
+		</div>
+	</head>
+	/*function over form
+	<br>
+	*make it unique to me
+	<br>
+	*ensure that it contains all the necessary information
+	*/
+</html>
