8ca9ec128110c731d27a8be0686dc76ff8fe8d05
Stephen Pelot <spelot@cnm.edu>
2018-11-01 08:10:38 -0600

    what's he that wishes so

---
+++ milestone-2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<div>
+			<h1>One Page Breakdown</h1>
+			<br>
+			<h2>Picture of me/paragraph about myself</h2>
+			<p>This section will show a bit more of my personal, as opposed to professional side, in order to put a face to the name
+			</p>
+			<br>
+			<h2>Quotes</h2>
+			<p>Quotes are important to me, as a way to show a person's convictions and beliefs concise, and elegant. I'll throw some of them in here as a good way to measure who I am with efficiency</p>
+			<br>
+			<h2>Projects/Experience</h2>
+			<p>This is where within the page my value as a developer can be ascertained. I'll have my capstone here, as well as any other things we may end up finishing during the course of class</p>
+			<br>
+			<h2>Pie chart</h2>
+			<p>This will be used to show my proficiencies and preferred languages. It should help to show employers whether or not I will be good for a certain job. It will be divided into the different languages, with larger pieces representing a better understanding of the knowledge</p>
+			<br>
+			<h2>Contact form/Links</h2>
+			<p>This is the call to action, once they've seen my site. It will have links to my Github repositories, my LinkedIn profile, and my StackOverflow account. The contact form will be a simple email relayer</p>
+		</div>
+		<img src="Wireframe%20for%20PWP.png">
+		<img src="Wireframe%20for%20PWP%20mobile.png"
+	</head>
+	/*function over form
+	<br>
+	*make it unique to me
+	<br>
+	*ensure that it contains all the necessary information
+	*/
+</html>
