dae021fd5b9594065c5b0d42e737db63277c4e72
Stephen Pelot <spelot@cnm.edu>
2018-11-15 10:52:44 -0700

    No, my fair cousin

--- milestone-2.php
+++ milestone-2.php
@@ -2,28 +2,32 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<div>
-			<h1>One Page Breakdown</h1>
-			<br>
-			<h2>Picture of me/paragraph about myself</h2>
-			<p>This section will show a bit more of my personal, as opposed to professional side, in order to put a face to the name
-			</p>
-			<br>
-			<h2>Quotes</h2>
-			<p>Quotes are important to me, as a way to show a person's convictions and beliefs concise, and elegant. I'll throw some of them in here as a good way to measure who I am with efficiency</p>
-			<br>
-			<h2>Projects/Experience</h2>
-			<p>This is where within the page my value as a developer can be ascertained. I'll have my capstone here, as well as any other things we may end up finishing during the course of class</p>
-			<br>
-			<h2>Pie chart</h2>
-			<p>This will be used to show my proficiencies and preferred languages. It should help to show employers whether or not I will be good for a certain job. It will be divided into the different languages, with larger pieces representing a better understanding of the knowledge</p>
-			<br>
-			<h2>Contact form/Links</h2>
-			<p>This is the call to action, once they've seen my site. It will have links to my Github repositories, my LinkedIn profile, and my StackOverflow account. The contact form will be a simple email relayer</p>
-		</div>
-		<img src="Wireframe-for-PWP.png">
-		<img src="Wireframe-for%20PWP-mobile.png">
+	<link rel="stylesheet" type="text/css" href="variable.css">
+
 	</head>
+	<body>
+		<div>
+		<h1>One Page Breakdown</h1>
+		<br>
+		<h2>Picture of me/paragraph about myself</h2>
+		<p>This section will show a bit more of my personal, as opposed to professional side, in order to put a face to the name
+		</p>
+		<br>
+		<h2>Quotes</h2>
+		<p>Quotes are important to me, as a way to show a person's convictions and beliefs concise, and elegant. I'll throw some of them in here as a good way to measure who I am with efficiency</p>
+		<br>
+		<h2>Projects/Experience</h2>
+		<p>This is where within the page my value as a developer can be ascertained. I'll have my capstone here, as well as any other things we may end up finishing during the course of class</p>
+		<br>
+		<h2>Pie chart</h2>
+		<p>This will be used to show my proficiencies and preferred languages. It should help to show employers whether or not I will be good for a certain job. It will be divided into the different languages, with larger pieces representing a better understanding of the knowledge</p>
+		<br>
+		<h2>Contact form/Links</h2>
+		<p>This is the call to action, once they've seen my site. It will have links to my Github repositories, my LinkedIn profile, and my StackOverflow account. The contact form will be a simple email relayer</p>
+	</div>
+	<img src="wireframe-for-pwp.png">
+	<img src="wireframe-for-pwp-mobile.png">
+	</body>
 	/*function over form
 	<br>
 	*make it unique to me
